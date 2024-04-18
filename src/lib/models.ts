e22d9c39ec0dc752917e65703e3c5ef92146c80b
BitWorldProtocol <cyptojune@gmail.com>
2024-04-18 10:20:07 +0800

    add mongoose models user and post

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -13,5 +13,44 @@ const userScema = new mongoose.Schema({
         required: true,
         unique: true,
         max: 50
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6
+    },
+    img: {
+        type: String
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
-})
+}, { timestamps: true })
+
+const postScema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, { timestamps: true })
+
+
+export const User = mongoose.models.User || mongoose.model("User", userScema)
+export const Post = mongoose.models.Post || mongoose.model("Post", postScema)
