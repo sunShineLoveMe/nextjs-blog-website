da838caa88f6be2b51d8b1bdf808fa97fabd94af
BitWorldProtocol <cyptojune@gmail.com>
2024-04-19 15:23:29 +0800

    add method delete

--- src/app/api/blog/[slug]/route.ts
+++ src/app/api/blog/[slug]/route.ts
@@ -3,6 +3,20 @@ import { connectToDb } from "@/lib/utils"
 import { NextApiRequest } from "next"
 import { NextResponse } from "next/server"
 
+
+/**
+ * NextApiRequest: 是一个用于处理 API 路由的请求对象的类型。
+ *  它提供了对客户端发起的 HTTP 请求的访问权限，包括请求的参数、头部、主体等信息
+ *  query：包含了请求中的查询参数。
+    body：包含了请求的主体数据，通常用于 POST、PUT 等请求方法。
+    headers：包含了请求的头部信息。
+    method：包含了请求的 HTTP 方法，比如 GET、POST 等。
+    cookies：包含了请求中的 cookie 数据。
+
+ * @param request: NextApiRequest
+ * @param param1 
+ * @returns 
+ */
 export const GET = async(request: NextApiRequest, { params }: {params: {slug: string}}) => {
     const { slug } = params
     try {
@@ -12,5 +26,17 @@ export const GET = async(request: NextApiRequest, { params }: {params: {slug: st
     } catch (error) {
         console.log(error)
         throw new Error("failed to fetch post!")
+    }
+}
+
+export const DELETE = async(request: NextApiRequest, { params }: {params: {slug: string}}) => {
+    const { slug } = params
+    try {
+        connectToDb()
+        await Post.deleteOne({slug});
+        return NextResponse.json("Post deleted")
+    } catch (error) {
+        console.log(error)
+        throw new Error("failed to delete post!")
     }
 }
